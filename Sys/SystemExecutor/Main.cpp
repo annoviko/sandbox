f264070e0fa2d38b364d8652930421fc9074d415
annoviko <spb.andr@yandex.ru>
2016-10-30 22:33:35 +0300

    [Sys.SystemExecutor] Close pipes.

--- Sys/SystemExecutor/Main.cpp
+++ Sys/SystemExecutor/Main.cpp
@@ -5,7 +5,13 @@
 
 int main(int argc, char * argv[]) {
     ExecutorResult result;
-    SystemExecutor::execute("pwd -a", result);
 
+    SystemExecutor::execute("pwd -a", result);
+    std::cout << result << std::endl;
+
+    SystemExecutor::execute("pwd", result);
+    std::cout << result << std::endl;
+
+    SystemExecutor::execute("ifconfig", result);
     std::cout << result << std::endl;
 }
--- Sys/SystemExecutor/SystemExecutor.cpp
+++ Sys/SystemExecutor/SystemExecutor.cpp
@@ -64,6 +64,9 @@ int SystemExecutor::execute(const std::string & pCommand, ExecutorResult & pOutp
         dup2(outputChannel[1], STDOUT_FILENO);  /* redirect output stream to output channel for parent */
         dup2(errorChannel[1], STDERR_FILENO);   /* redirect error stream to error channel for parent */
 
+        close(outputChannel[1]);
+        close(errorChannel[1]);
+
         execl("/bin/sh", "sh", "-c", pCommand.c_str(), (char *) 0);
 
         /* shouldn't be reached in case of execl success */
@@ -76,6 +79,9 @@ int SystemExecutor::execute(const std::string & pCommand, ExecutorResult & pOutp
 
     readChannel(outputChannel[0], pOutputResult.getTextResult());
     readChannel(errorChannel[0], pOutputResult.getTextError());
+
+    close(outputChannel[0]);    /* close read end of channel */
+    close(errorChannel[0]);     /* close read end of channel */
 
     int returnValue = 0;
     waitpid(childProcessId, &returnValue, 0);
