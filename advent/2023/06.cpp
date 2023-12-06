415bc68daf6f714e6a98a213a289304bd8905991
annoviko <spb.andr@yandex.ru>
2023-12-06 22:44:18 +0100

    [advent] 06 - solution based on math.

--- advent/2023/06.cpp
+++ advent/2023/06.cpp
@@ -11,6 +11,13 @@ struct record {
 };
 
 
+enum class algorithm {
+    BRUTE_FORCE = 0,        /* O(N)         */
+    BINARY_SEARCH = 1,      /* O(log(N))    */
+    MATH = 2                /* O(1)         */
+};
+
+
 class record_analyser {
 private:
     record m_entry;
@@ -20,7 +27,21 @@ public:
 
 
 public:
-    std::uint64_t ways_to_win() {
+    std::uint64_t ways_to_win(const algorithm& method) {
+        switch (method) {
+        case algorithm::BRUTE_FORCE:
+            return ways_to_win_brute_force();
+
+        case algorithm::BINARY_SEARCH:
+            return ways_to_win_binary_search();
+
+        case algorithm::MATH:
+            return ways_to_win_math();
+        }
+    }
+
+
+    std::uint64_t ways_to_win_brute_force() {   /* O(N) */
         std::uint64_t ways_to_win = 0;
 
         for (int i = 1; i < m_entry.time; i++) {
@@ -36,7 +57,7 @@ public:
     }
 
 
-    std::uint64_t ways_to_win_binary_search() {
+    std::uint64_t ways_to_win_binary_search() { /* O(log(N)) */
         std::uint64_t ways_to_win = 0;
 
         /* find left border */
@@ -75,6 +96,41 @@ public:
 
         std::uint64_t right = lborder;   /* point to value inside (the last value greater than the record) */
         return right - left + 1;    /* both inside, so add +1 as zero shift (for example, 5, 6, 7 - the amount is 3, not 2) */
+    }
+
+
+    std::uint64_t ways_to_win_math() {  /* O(1) */
+        /* 
+		
+		s[n] = v[n] * (t - n) = n * (t - n)
+		
+		we know `s` and `t` for the record, so we can build an equation:
+		
+		s = x * (t - x)
+		
+		x^2 - t * x + s = 0
+		
+		We need to know length between x1 and x2.
+		
+		*/
+
+        auto D = [](double t, double s) -> double {
+            return std::pow(t, 2) - 4.0 * s;
+        };
+
+        auto X1 = [&D](double t, double s) {
+            return (t + std::sqrt(D(t, s))) / 2.0;
+        };
+
+        auto X2 = [&D](double t, double s) {
+            return (t - std::sqrt(D(t, s))) / 2.0;
+        };
+
+        std::int64_t x1 = std::ceil(X1(m_entry.time, m_entry.distance) - 1);    /* right border - correct to be inside */
+        std::int64_t x2 = std::floor(X2(m_entry.time, m_entry.distance) + 1);   /* left border - correct to be inside  */
+
+        std::int64_t length = x1 - x2 + 1;  /* both inside, so add +1 as zero shift (for example, 5, 6, 7 - the amount is 3, not 2) */
+        return length;
     }
 
 private:
@@ -122,11 +178,11 @@ public:
     adviser(const std::vector<record>& p_records) : m_records(p_records) { }
 
 public:
-    std::uint64_t ways_to_win() {
+    std::uint64_t ways_to_win(const algorithm& method) {
         int ways_to_win = 0;
 
         for (const auto& r : m_records) {
-            const std::uint64_t ways = record_analyser(r).ways_to_win_binary_search();
+            const std::uint64_t ways = record_analyser(r).ways_to_win(method);
             if (ways > 0) {
                 if (ways_to_win == 0) {
                     ways_to_win = 1;
@@ -157,8 +213,8 @@ record correct_input(const std::vector<record>& records) {
 int main() {
     auto input = read_input();
 
-    std::cout << "Multiplication of number of ways you can beat the record: " << adviser(input).ways_to_win() << std::endl;
-    std::cout << "Number of ways you can beat the record: " << record_analyser(correct_input(input)).ways_to_win_binary_search() << std::endl;
+    std::cout << "Multiplication of number of ways you can beat the record: " << adviser(input).ways_to_win(algorithm::MATH) << std::endl;
+    std::cout << "Number of ways you can beat the record: " << record_analyser(correct_input(input)).ways_to_win(algorithm::MATH) << std::endl;
 
     return 0;
 }
