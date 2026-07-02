ce31c74f49617f81d1255803107b7804dd425b43
annoviko <spb.andr@yandex.ru>
2026-07-02 16:03:22 +0200

    [leetcode] 3286.

--- leetcode/3286 - Find a Safe Walk Through a Grid.cpp
+++ leetcode/3286 - Find a Safe Walk Through a Grid.cpp
@@ -1,6 +1,12 @@
 #include <vector>
 #include <queue>
+#include <deque>
 
+#if 0
+    #define DIJKSTRA
+#else
+    #define BFS_1_0
+#endif
 
 struct entry {
     int row = -1;
@@ -19,20 +25,16 @@ struct comparator {
 class Solution {
 public:
     bool findSafeWalk(const std::vector<std::vector<int>>& grid, int health) {
+#if defined(DIJKSTRA)   /* Total: O(N * M * log(N * M)) */
         std::priority_queue<entry, std::vector<entry>, comparator> pq;
         std::vector<std::vector<int>> dist(grid.size(), std::vector<int>(grid[0].size(), INT_MAX));
-        std::vector<std::vector<int>> visited(grid.size(), std::vector<int>(grid[0].size(), false));
 
         pq.push({ 0, 0, grid[0][0] });
         dist[0][0] = grid[0][0];
 
-        while (!pq.empty()) {
-            entry cur = pq.top();
-            pq.pop();
-
-            if (visited[cur.row][cur.col]) {
-                continue;
-            }
+        while (!pq.empty()) {   /* O(N * M) */
+            entry cur = pq.top();   /* O(1) */
+            pq.pop();               /* O(log(N * M)) */
 
             if (cur.cost > dist[cur.row][cur.col])
                 continue;
@@ -40,8 +42,6 @@ public:
             if (cur.row == grid.size() - 1 && cur.col == grid[0].size() - 1) {
                 break;
             }
-
-            visited[cur.row][cur.col] = true;
 
             const std::vector<std::pair<int, int>> neis = {
                 { cur.row - 1, cur.col },
@@ -55,10 +55,6 @@ public:
                     continue;
                 }
 
-                if (visited[nei.first][nei.second]) {
-                    continue;
-                }
-
                 const int candidate = cur.cost + grid[nei.first][nei.second];
                 if (candidate >= health) {
                     continue;
@@ -66,7 +62,7 @@ public:
 
                 if (candidate < dist[nei.first][nei.second]) {
                     dist[nei.first][nei.second] = candidate;
-                    pq.push({ nei.first, nei.second, candidate });
+                    pq.push({ nei.first, nei.second, candidate });  /* O(log(N * M)) */
                 }
             }
         }
@@ -77,5 +73,55 @@ public:
         }
 
         return false;
+
+#elif defined(BFS_1_0)
+        std::deque<entry> q;
+        q.push_back({ 0, 0, grid[0][0] });
+
+        std::vector<std::vector<int>> dist(grid.size(), std::vector<int>(grid[0].size(), INT_MAX));
+        dist[0][0] = grid[0][0];
+
+        if (dist[0][0] >= health) {
+            return false;
+        }
+
+        while (!q.empty()) {    /* O(N * M) */
+            const entry cur = q.front();    /* O(1) */
+            q.pop_front();                  /* O(1) */
+
+            if (cur.row == grid.size() - 1 && cur.col == grid[0].size() - 1) {
+                return true;
+            }
+
+            const std::vector<std::pair<int, int>> neis = {
+                { cur.row - 1, cur.col },
+                { cur.row + 1, cur.col },
+                { cur.row, cur.col - 1 },
+                { cur.row, cur.col + 1 }
+            };
+
+            for (const auto& nei : neis) {
+                if (nei.first < 0 || nei.first >= grid.size() || nei.second < 0 || nei.second >= grid[0].size()) {
+                    continue;
+                }
+
+                const int candidate = cur.cost + grid[nei.first][nei.second];
+                if (candidate >= dist[nei.first][nei.second]) {
+                    continue;
+                }
+
+                dist[nei.first][nei.second] = candidate;
+
+                if (grid[nei.first][nei.second] == 0) {
+                    q.push_front({ nei.first, nei.second, cur.cost });      /* O(1) amortized */
+                }
+                else if (cur.cost + 1 < health) {
+                    q.push_back({ nei.first, nei.second, cur.cost + 1 });   /* O(1) */
+                }
+            }
+        }
+
+        return false;
+#endif
     }
 };
